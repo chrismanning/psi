be979b62f2d93b78d7c29780dcf85e853c94f588
Chris Manning <cmanning999@gmail.com>
2019-08-31 16:24:37 +0100

    pub use TriggerId

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ pub mod monitor;
 pub mod psi;
 pub mod trigger;
 
-pub use error::{PsiError, Result};
-pub use monitor::PsiMonitor;
 pub use crate::psi::{AllPsiStats, Psi, PsiKind, PsiLine};
+pub use error::{PsiError, Result};
+pub use monitor::{PsiMonitor, TriggerId};
 pub use trigger::Trigger;
